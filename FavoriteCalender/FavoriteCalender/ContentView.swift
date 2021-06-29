f4116421a0eea30b68aeb1ed143b16535a4d5ef5
hira <hirapoke823@gmail.com>
2021-06-30 07:20:23 +0900

    新規プロジェクト作成

---
+++ FavoriteCalender/FavoriteCalender/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FavoriteCalender
+//
+//  Created by 平島峻成 on 2021/06/30.
+//  Copyright © 2021 平島峻成. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
